9579e02505a3a08cc7b95dac94042cb933c25afb
mtelias2 <moutazelias@hotmail.com>
2022-05-24 20:14:40 +0400

    created the scrapper class

---
+++ src/Data Collection/webscrapper.py
@@ -0,0 +1,9 @@
+#Written by Moutaz Elias
+#Scraps housing price and rent data from https://www.propertyfinder.ae/
+#and stores it in a csv file
+#written using OOP classes and functions
+
+class Scrapper:
+    self.__init__(self,link):
+        
+
